d3dd353c294d487c602782a4feaf7657212ebf12
flavioepimentel <flavio.ericksan@hotmail.com>
2022-03-16 15:05:14 -0300

    Implementa funções basicas do banco

--- Python-Bank/conta.py
+++ Python-Bank/conta.py
@@ -1,20 +1,46 @@
 
+from operator import truediv
+
+
 class ContaCorrente:
     def __init__(self, numero, titular, saldo, limite):
         print("Construindo objeto {}".format(self))
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
         
     def extrato(self):
-        print("Saldo disponível: {}".format(str(self.saldo)))
+        print("Saldo disponível: {}".format(str(self.__saldo)))
+
+    def saldo_disponivel(self, valor):
+        if self.__saldo - valor < 0:
+            return False
+        else:
+            return True
 
     def saque(self, valor):
-        if self.saldo - valor < 0:
-            print('Valor não disponível')
+        if self.saldo_disponivel(valor):
+            self.__saldo -= valor
         else:
-            self.saldo -= valor
+            print('Valor não disponível')
 
     def deposito(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
+    
+    def transfere(self, valor, destino):
+        self.saque(valor)
+        destino.deposito(valor)
+
+    def get_numero(self):
+        return self.__numero
+
+    def get_titular(self):
+        return self.__titular
+
+    def get_limite(self):
+        return self.__limite
+
+    def set_limite(self, valor):
+        self.__limite = valor
+
