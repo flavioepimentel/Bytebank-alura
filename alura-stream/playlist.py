2a6c613afcbbdb7ef608eb692a5302997f23e874
flavioepimentel <flavio.ericksan@hotmail.com>
2022-03-17 12:52:28 -0300

    Transforma classe em sized e iteravel

--- alura-stream/playlist.py
+++ alura-stream/playlist.py
@@ -1,10 +1,52 @@
-from videos import Programa
+from videos import Filmes, Series, Programa
 
 
-class Playlist(list):
+class Playlist():
     def __init__(self, nome, programas):
         self.nome = nome
-        super().__init__(programas)
+        self.__programas = programas
+
+    def __len__(self):
+        return len(self.__programas)
+
+    def __getitem__(self, item):
+        return self.__programas[item]        
 
     def addPrograma(self):
         pass
+
+
+
+vingador = Filmes("vingadores - guerra infinita", 2019, "Homem de ferro, Homem aranha", "Aventura", "Um filme de sucesso", 2)
+robot = Series("Mr Robot", 2019, "Sr Robot", "Ação", "Sériado de hacker", 5)
+
+robot.newLike()
+robot.newLike()
+robot.newLike()
+robot.newLike()
+robot.newLike()
+robot.newLike()
+robot.newLike()
+robot.newLike()
+robot.newLike()
+
+vingador.newLike()
+vingador.newLike()
+vingador.newLike()
+vingador.newLike()
+vingador.newLike()
+vingador.newLike()
+vingador.newLike()
+vingador.newLike()
+vingador.newLike()
+vingador.newLike()
+vingador.newLike()
+vingador.newLike()
+vingador.newLike()
+
+
+
+lista = [vingador, robot]
+
+for programa in lista:
+    print(programa)
